5c9e90d512bbbf6e8ea72091f011e02171176a07
Asya <web.zaslavskaya@gmail.com>
2023-05-25 18:42:12 +0300

    catalog-one page desktop without gallery

---
+++ dist/js/catalog-one.js
@@ -0,0 +1,54 @@
+console.log('catalog ONE');
+
+$('.catalog-slider').slick({
+    dots: true,
+    // centerMode: true,
+    focusOnSelect: true,
+    arrows: true,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    customPaging: function(slider, i) {
+        return '<div class="thumbnails">' +$(slider.$slides[i]).find('img').prop('outerHTML')+ '</div>';
+    }
+});
+
+
+
+function Marquee(selector, speed) {
+    const parentSelector = document.querySelector(selector);
+    const clone = parentSelector.innerHTML;
+    const firstElement = parentSelector.children[0];
+    let i = 0;
+    console.log(firstElement);
+    // parentSelector.insertAdjacentHTML('beforeend', clone);
+  
+    setInterval(function () {
+      firstElement.style.marginLeft = `-${i}px`;
+      if (i > firstElement.clientWidth) {
+        i = 0;
+      }
+      i = i + speed;
+    }, 0);
+  }
+  
+  //after window is completed load
+  //1 class selector for marquee
+  //2 marquee speed 0.2
+  window.addEventListener('load', Marquee('.marquee', 0.6))
+  
+  
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    });
+  }
+//# sourceMappingURL=catalog-one.js.map
---
+++ src/js/header.js
@@ -0,0 +1,32 @@
+console.log(`header js`);
+
+
+let openMenuBtn = document.querySelector('.menu-button');
+let closeMenuBtn = document.querySelector('.close-button');
+
+let headerIs = document.querySelector('.header');
+
+openMenuBtn.addEventListener('click', ()=>{
+    headerIs.classList.add('open-header')
+});
+
+closeMenuBtn.addEventListener('click', ()=>{
+    headerIs.classList.remove('open-header')
+});
+
+let dropsBtns = document.querySelectorAll('.dropup');
+
+
+dropsBtns.forEach((item, e)=>{
+    item.addEventListener('click', (e)=>{
+        console.log(item);
+        
+        if(item.classList.contains("open")){
+            item.classList.remove("open");
+        } else {
+            item.classList.add("open");
+
+        }
+
+    })
+})
