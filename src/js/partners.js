32aaca8d6f3b470dcc6aae618e914b9b41296bd3
Asya <web.zaslavskaya@gmail.com>
2023-06-01 14:49:50 +0300

    page partners

---
+++ src/js/partners.js
@@ -0,0 +1,21 @@
+console.log('parnters');
+
+
+let input = document.querySelector("#phone");
+window.intlTelInput(input, {
+  showFlags: true,
+  separateDialCode: true,
+  allowDropdown: true,
+  preferredCountries: ["ua","pl", "md" ],
+  utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/utils.js",
+});
+
+
+let input2 = document.querySelector("#phone2");
+window.intlTelInput(input2, {
+  showFlags: true,
+  separateDialCode: true,
+  allowDropdown: true,
+  preferredCountries: ["ua","pl", "md" ],
+  utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/utils.js",
+});
