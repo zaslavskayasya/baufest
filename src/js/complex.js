6723f342a34330874afcdeda3de29de3812c975a
Asya <web.zaslavskaya@gmail.com>
2023-06-01 18:57:00 +0300

    complex page

---
+++ src/js/complex.js
@@ -0,0 +1,37 @@
+console.log('complex');
+
+$('.center-slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+
+  let input = document.querySelector("#phone");
+  window.intlTelInput(input, {
+    showFlags: true,
+    separateDialCode: true,
+    allowDropdown: true,
+    preferredCountries: ["ua","pl", "md" ],
+    utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/utils.js",
+  });
