92788045f2e406d802dbe46fc82d81b3a2557e73
Asya <web.zaslavskaya@gmail.com>
2023-06-05 00:01:14 +0300

    gallery and gallery one pages

---
+++ dist/js/gallery.js
@@ -0,0 +1,52 @@
+console.log('gallery');
+
+document.addEventListener("DOMContentLoaded", function() {
+    const themeFilters = document.getElementsByClassName("theme-filter");
+    const casesContainer = document.querySelector(".case-container");
+    const cases = Array.from(document.getElementsByClassName("case"));
+
+    Array.from(themeFilters).forEach(function(themeFilter) {
+        themeFilter.addEventListener("click", function() {
+            const selectedTheme = themeFilter.getAttribute("data-theme");
+
+            Array.from(themeFilters).forEach(function(filter) {
+                filter.classList.remove("active");
+            });
+
+            themeFilter.classList.add("active");
+
+            cases.forEach(function(caseElement) {
+                const caseTheme = caseElement.getAttribute("data-theme");
+
+                if (selectedTheme === "all" || selectedTheme === caseTheme) {
+                    caseElement.classList.remove("hide");
+                } else {
+                    caseElement.classList.add("hide");
+                }
+            });
+
+            rearrangeCases();
+        });
+    });
+
+    function rearrangeCases() {
+        const visibleCases = cases.filter(function(caseElement) {
+            return !caseElement.classList.contains("hide");
+        });
+
+        const hiddenCases = cases.filter(function(caseElement) {
+            return caseElement.classList.contains("hide");
+        });
+
+        casesContainer.innerHTML = "";
+
+        visibleCases.forEach(function(caseElement) {
+            casesContainer.appendChild(caseElement);
+        });
+
+        hiddenCases.forEach(function(caseElement) {
+            casesContainer.appendChild(caseElement);
+        });
+    }
+});
+//# sourceMappingURL=gallery.js.map
---
+++ dist/js/gallery_one.js
@@ -0,0 +1,14 @@
+console.log('one gallery');
+
+$('.gallery-type2').slick({
+    dots: true,
+    // centerMode: true,
+    focusOnSelect: true,
+    arrows: true,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    customPaging: function(slider, i) {
+        return '<div class="thumbnails2">' +$(slider.$slides[i]).find('img').prop('outerHTML')+ '</div>';
+    }
+});
+//# sourceMappingURL=gallery_one.js.map
